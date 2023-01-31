f4ff445c0c6a03c075cd7aea0dd61aa357a273c3
Zi Hang <kevu.lee@gmail.com>
2023-01-31 12:04:26 +0800

    refactor: change app name

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from "react"
 function Header() {
     return (
         <div className="flex text-center justify-center font-inter font-black text-2xl text-secondary py-5">
-            FINGOALS
+            REFINANCE
         </div>
     )
 }
